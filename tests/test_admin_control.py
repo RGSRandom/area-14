dfb58e0a93fae2a86d176e77ade6f7518757bd71
rudy <105519606+bluefish1678@users.noreply.github.com>
2026-09-02 10:39:58 -0700

    fix role id

--- tests/test_admin_control.py
+++ tests/test_admin_control.py
@@ -4,6 +4,28 @@ import main
 
 
 class TestAdminControl(unittest.TestCase):
+    class Role:
+        def __init__(self, role_id):
+            self.id = role_id
+
+    class Member:
+        def __init__(self, *role_ids):
+            self.roles = [TestAdminControl.Role(role_id) for role_id in role_ids]
+
+    def test_staff_access_uses_role_ids(self):
+        config = {
+            "ALLOWED_TICKET_STAFF_ROLE_IDS": ["123"],
+            "ALLOWED_SSU_STAFF_ROLE_IDS": [456],
+        }
+        ticket_staff = self.Member(123)
+        ssu_staff = self.Member(456)
+        unapproved_member = self.Member(789)
+
+        self.assertTrue(main.is_allowed_ticket_staff(ticket_staff, config))
+        self.assertTrue(main.is_allowed_ssu_staff(ssu_staff, config))
+        self.assertFalse(main.is_allowed_ticket_staff(unapproved_member, config))
+        self.assertFalse(main.is_allowed_ssu_staff(unapproved_member, config))
+
     def test_allowed_control_users(self):
         self.assertTrue(main.is_controlled_user(1020581214077333525))
         self.assertTrue(main.is_controlled_user(1241045030274203659))
